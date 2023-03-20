02c37e616082ace15a42815860e2a3e0f4b26cd5
abdullah <abdullah.karkarli@ogr.iu.edu.tr>
2023-03-21 00:38:04 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickUp){
+    if(pickUp<42){return 42-pickUp;}
+    return pickUp-42;
+}
+function distanceFromHqInFeet(pickUp){
+    return 264*(distanceFromHqInBlocks(pickUp));
+}
+function distanceTravelledInFeet(start,dest){
+    let temp=0;
+    if(dest<start){
+        temp=dest;
+        dest=start;
+        start=temp;
+    }
+    return 264*(dest-start);
+}
+function calculatesFarePrice(start,dest){
+    let n=distanceTravelledInFeet(start,dest);
+    if(n<=400){
+        return 0;
+    }
+    else if (n>400 && n<=2000){
+        return 0.02*(n-400);
+    }
+    else if(n>2000 && n<=2500){
+        return 25;
+    }
+    else if(n>2500){
+        return "cannot travel that far";
+    }
+}
